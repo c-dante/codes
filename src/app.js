df6a8dc75e08da5afccfb90400529c5e597a7953
dante <c.dante.federici@gmail.com>
2017-10-15 14:11:16 -0400

    Removed unused log

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ import dfLanding from './landing';
 const APP_MODULE = 'my-app';
 
 angular.module(APP_MODULE, [dfLanding])
-	.run([() => {
-		console.debug('angular.run', 'Bootstrap runtime modules like redux or a router');
-	}]);
+	// .run([() => {
+	// 	console.debug('angular.run', 'Bootstrap runtime modules like redux or a router');
+	// }]);
 
 // When we have the document, bootstrap our application
 angular.element(document).ready(() => {
